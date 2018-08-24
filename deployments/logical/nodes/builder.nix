415c2ab2e8334c56e4778091cb9ab08f0b3dbe04
Christian Höppner <christian.hoppner@serokell.io>
2018-08-24 18:01:37 +0100

    move dependencies around

--- deployments/logical/nodes/builder.nix
+++ deployments/logical/nodes/builder.nix
@@ -8,7 +8,7 @@ let keys = config.dscp.keys; in
     enable = true;
     name = "dscp-runner";
     package = pkgs.buildkite-agent3;
-    runtimePackages = with pkgs; [ bash nix gnutar gzip nixops-git ];
+    runtimePackages = with pkgs; [ bash nix  ];
     tokenPath = toString keys.buildkite-token;
     meta-data = "queue=dscp,nix=true,nixops=true";
     openssh.privateKeyPath = toString keys.buildkite-ssh-private;
