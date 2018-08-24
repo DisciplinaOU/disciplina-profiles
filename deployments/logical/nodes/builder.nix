99c822e1525753df0ba98750a480a4c15863b051
Christian Höppner <christian.hoppner@serokell.io>
2018-08-24 15:01:15 +0100

    add nixops to bk agent runtime

--- deployments/logical/nodes/builder.nix
+++ deployments/logical/nodes/builder.nix
@@ -5,9 +5,9 @@ let keys = config.dscp.keys; in
     enable = true;
     name = "dscp-runner";
     package = pkgs.buildkite-agent3;
-    runtimePackages = with pkgs; [ bash nix gnutar gzip ];
+    runtimePackages = with pkgs; [ bash nix gnutar gzip nixops-git ];
     tokenPath = toString keys.buildkite-token;
-    meta-data = "queue=dscp,nix=true";
+    meta-data = "queue=dscp,nix=true,nixops=true";
     openssh.privateKeyPath = toString keys.buildkite-ssh-private;
     openssh.publicKeyPath = toString keys.buildkite-ssh-public;
     extraConfig = ''
