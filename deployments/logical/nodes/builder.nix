8555130953c4e65637125b0b7c20cbf7cdc2af80
Christian Höppner <christian.hoppner@serokell.io>
2018-08-24 21:35:56 +0100

    fix the derp again

--- deployments/logical/nodes/builder.nix
+++ deployments/logical/nodes/builder.nix
@@ -13,7 +13,7 @@ let keys = config.dscp.keys; in
     package = pkgs.buildkite-agent3;
     runtimePackages = with pkgs; [
       # Basics
-      bash nix gnutar gunzip
+      bash nix gnutar gzip
       # git checkout fails without this because .gitattributes defines it as clean/smudge filter
       git-crypt
     ];
