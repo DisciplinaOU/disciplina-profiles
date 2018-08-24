62e484eb7d0f44ff41c055bbad747a7d1794106f
Christian Höppner <christian.hoppner@serokell.io>
2018-08-24 21:24:43 +0100

    derp: tar and gunzip

--- deployments/logical/nodes/builder.nix
+++ deployments/logical/nodes/builder.nix
@@ -13,7 +13,7 @@ let keys = config.dscp.keys; in
     package = pkgs.buildkite-agent3;
     runtimePackages = with pkgs; [
       # Basics
-      bash nix
+      bash nix tar gunzip
       # git checkout fails without this because .gitattributes defines it as clean/smudge filter
       git-crypt
     ];
