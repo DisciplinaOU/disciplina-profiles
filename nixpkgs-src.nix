2fe1d5c3958c453072383eca1c14965ee8c960d0
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-08-27 19:10:57 +0300

    Use serokell nixpkgs

--- nixpkgs-src.nix
+++ nixpkgs-src.nix
@@ -1,4 +1,1 @@
-fetchTarball {
-  url = https://github.com/NixOS/nixpkgs/archive/8395f9aa85e621c076334a67d814de8221ce7983.tar.gz;
-  sha256 = "00vqapzvn9mnyj4ak5i7wnv5dylnc3dddbd43643fnxbw85zb3kp";
-}
+fetchTarball https://github.com/serokell/nixpkgs
