15e0f011d0b4926cef6c78c415401af1bc3134fa
Christian Höppner <christian.hoppner@serokell.io>
2018-08-27 17:21:10 +0100

    disable BK agent on builder for testing purposes

--- deployments/logical/nodes/builder.nix
+++ deployments/logical/nodes/builder.nix
@@ -7,24 +7,24 @@ let keys = config.dscp.keys; in
   # To copy closures around
   nix.trustedUsers = [ "chris" ];
 
-  services.buildkite-agent = {
-    enable = true;
-    name = "dscp-runner";
-    package = pkgs.buildkite-agent3;
-    runtimePackages = with pkgs; [
-      # Basics
-      bash nix gnutar gzip
-      # git checkout fails without this because .gitattributes defines it as clean/smudge filter
-      git-crypt
-    ];
-    tokenPath = toString keys.buildkite-token;
-    meta-data = "queue=dscp,nix=true,nixops=true";
-    openssh.privateKeyPath = toString keys.buildkite-ssh-private;
-    openssh.publicKeyPath = toString keys.buildkite-ssh-public;
-    extraConfig = ''
-      shell="${pkgs.bash}/bin/bash -e -c"
-    '';
-  };
+  # services.buildkite-agent = {
+  #   enable = true;
+  #   name = "dscp-runner";
+  #   package = pkgs.buildkite-agent3;
+  #   runtimePackages = with pkgs; [
+  #     # Basics
+  #     bash nix gnutar gzip
+  #     # git checkout fails without this because .gitattributes defines it as clean/smudge filter
+  #     git-crypt
+  #   ];
+  #   tokenPath = toString keys.buildkite-token;
+  #   meta-data = "queue=dscp,nix=true,nixops=true";
+  #   openssh.privateKeyPath = toString keys.buildkite-ssh-private;
+  #   openssh.publicKeyPath = toString keys.buildkite-ssh-public;
+  #   extraConfig = ''
+  #     shell="${pkgs.bash}/bin/bash -e -c"
+  #   '';
+  # };
 
   # Make sure admins can read/write the nixops state file to allow wrapper script access
   users.extraGroups.nixops = {};
@@ -42,9 +42,9 @@ let keys = config.dscp.keys; in
   };
 
   dscp.keys = {
-    buildkite-token =       { services = [ "buildkite-agent" ]; user = "buildkite-agent"; };
-    buildkite-ssh-private = { services = [ "buildkite-agent" ]; user = "buildkite-agent"; };
-    buildkite-ssh-public =  { services = [ "buildkite-agent" ]; user = "buildkite-agent"; };
+    # buildkite-token =       { services = [ "buildkite-agent" ]; user = "buildkite-agent"; };
+    # buildkite-ssh-private = { services = [ "buildkite-agent" ]; user = "buildkite-agent"; };
+    # buildkite-ssh-public =  { services = [ "buildkite-agent" ]; user = "buildkite-agent"; };
     aws-credentials =   rec { services = [ "buildkite-agent" ]; user = "buildkite-agent"; shared = false; } ;
   };
 }
