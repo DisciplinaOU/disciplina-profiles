d80ad24e3274cc482e3b5d9df670802b5b9c60e1
Christian Höppner <christian.hoppner@serokell.io>
2018-08-28 00:27:42 +0100

    missing logical deployment for witness zero

--- deployments/logical/default.nix
+++ deployments/logical/default.nix
@@ -4,6 +4,7 @@
   defaults = import ../../modules;
 
   builder  = import ./nodes/builder.nix;
+  witness0 = import ./nodes/witness.nix { n = 0; };
   witness1 = import ./nodes/witness.nix { n = 1; };
   witness2 = import ./nodes/witness.nix { n = 2; };
   witness3 = import ./nodes/witness.nix { n = 3; };
