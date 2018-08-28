4eb9865fba013f39a53a5d4dfa56b55b76bf400b
Christian Höppner <christian.hoppner@serokell.io>
2018-08-28 13:45:19 +0100

    tell nixops to use private network to reach witnesses

--- deployments/physical/aws/nodes/witness.nix
+++ deployments/physical/aws/nodes/witness.nix
@@ -4,7 +4,8 @@
   deployment.ec2 = {
     instanceType = "t2.small";
     ebsInitialRootDiskSize = 30;
-    usePrivateIpAddress = lib.mkIf internal true;
+    # Witness nodes don't allow SSH on public interface
+    usePrivateIpAddress = true;
     securityGroupIds = [ resources.ec2SecurityGroups.dscp-ssh-private-sg.name ]
       ++ (if internal then
       [ resources.ec2SecurityGroups.dscp-witness-private-sg.name ]
