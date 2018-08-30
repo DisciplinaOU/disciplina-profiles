ed0a910acbb4b9162336b251f27096866042c74e
Christian Höppner <christian.hoppner@serokell.io>
2018-08-30 20:58:04 +0100

    let operators manage disciplina-* systemd services

--- modules/default.nix
+++ modules/default.nix
@@ -73,19 +73,18 @@ in
   programs.zsh.enable = true;
   programs.mosh.enable = true;
 
-  security.sudo.wheelNeedsPassword = false;
-
   security.sudo = {
+    wheelNeedsPassword = false;
     extraRules = [
       {
       ##
       # Allow members of the `nixops` group, as well as user `buildkite-agent`
       commands = [
-        { command = "systemctl start disciplina-*";
+        { command = "${pkgs.systemd}/bin/systemctl start disciplina-*";
           options = [ "NOPASSWD" ]; }
-        { command = "systemctl stop disciplina-*";
+        { command = "${pkgs.systemd}/bin/systemctl stop disciplina-*";
           options = [ "NOPASSWD" ]; }
-        { command = "systemctl restart disciplina-*";
+        { command = "${pkgs.systemd}/bin/systemctl restart disciplina-*";
           options = [ "NOPASSWD" ]; }
       ];
       groups = [ "wheel" "nixops" ];
@@ -154,6 +153,7 @@ in
 
   users.mutableUsers = false;
 
+  users.extraGroups.nixops = {};
   users.users = lib.mapAttrs expandUser (import ../keys/ssh.nix);
 
   nixpkgs.config.allowUnfree = true;
