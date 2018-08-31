448b711e10892f86c52d535f93aa7bd94fdb0887
Christian Höppner <christian.hoppner@serokell.io>
2018-08-31 18:42:20 +0100

    refer to disciplina by fetchGit instead of nix path

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -11,8 +11,8 @@ rec {
     inherit (final.python2Packages) libvirt;
   };
 
-  # disciplina = (import (fetchGit { url = "ssh://git@github.com:/DisciplinaOU/disciplina"; ref = "release-alpha"; })).disciplina-bin;
-  disciplina = (import <disciplina>);
+  disciplina = (import (fetchGit { url = "ssh://git@github.com:/DisciplinaOU/disciplina"; ref = "release-alpha"; }));
+  # disciplina = (import <disciplina>);
   disciplina-bin = disciplina.disciplina-bin;
   disciplina-faucet-frontend = disciplina.disciplina-faucet-frontend;
   disciplina-witness-frontend = disciplina.disciplina-witness-frontend;
