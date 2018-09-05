39a216f6cf12af8532ba21a0337777d0945bc956
Christian Höppner <christian.hoppner@serokell.io>
2018-09-05 19:43:03 +0100

    Point disciplina at git

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -12,8 +12,8 @@ rec {
     inherit (final.python2Packages) libvirt;
   };
 
-  # disciplina = (import (fetchGit { url = "ssh://git@github.com:/DisciplinaOU/disciplina"; ref = "master"; }));
-  disciplina = (import <disciplina>);
+  disciplina = (import (fetchGit { url = "ssh://git@github.com:/DisciplinaOU/disciplina"; ref = "master"; }));
+  # disciplina = (import <disciplina>);
   disciplina-bin = disciplina.disciplina-bin;
   disciplina-faucet-frontend = disciplina.disciplina-faucet-frontend;
   disciplina-witness-frontend = disciplina.disciplina-witness-frontend;
