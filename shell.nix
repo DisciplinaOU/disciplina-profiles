f23c145252bb48e980b7c60c942b27b26cd0e1b3
Christian Höppner <christian.hoppner@serokell.io>
2018-09-05 16:29:33 +0100

    Remove sshfs from shell.nix

--- shell.nix
+++ shell.nix
@@ -11,7 +11,7 @@ in
 
 stdenv.mkDerivation {
   name = "dscp-ops-env";
-  buildInputs = with pkgs; [ nixops-git git-crypt sshfs ];
+  buildInputs = with pkgs; [ nixops-git git-crypt ];
 
   NIX_PATH = builtins.concatStringsSep ":" [
     "nixpkgs=${toString pkgs.path}"
