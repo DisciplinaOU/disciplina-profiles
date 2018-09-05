561139076f7b1a745a782e3ee3fefb3aa1612095
Christian Höppner <christian.hoppner@serokell.io>
2018-09-05 16:30:38 +0100

    New deployment to test PR

---
+++ deployments/physical/aws/dscp206.nix
@@ -0,0 +1,11 @@
+import ./. rec {
+  accessKeyId = "srk-staging";
+  domain = "dscp206.dscp.serokell.review";
+  realDomain = "dscp206.dscp.serokell.review";
+  DNSZone = "serokell.review";
+  faucetUrl = "http://faucet.dscp206.dscp.serokell.review";
+  witnessUrl = "http://witness.dscp206.dscp.serokell.review";
+  keydir = "testing";
+  region = "eu-west-2";
+  zone = "${region}a";
+}
