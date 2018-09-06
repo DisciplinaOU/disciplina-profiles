db4373776b1eff2eed14f7cc30270b5ea3d4b6ad
Christian Höppner <christian.hoppner@serokell.io>
2018-09-06 19:02:59 +0100

    Whoops quoting

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 NIXOPS_OPTIONS='-I ssh-key=/var/lib/nixops/.ssh/id_rsa'
 SLEEP='15m'
-sudo -u nixops -- sh -c 'nixops deploy $NIXOPS_OPTIONS --include witness0 builder; sleep $SLEEP; nixops deploy $NIXOPS_OPTIONS --exclude witness0 builder'
+sudo -u nixops -- sh -c "nixops deploy $NIXOPS_OPTIONS --include witness0 builder; sleep $SLEEP; nixops deploy $NIXOPS_OPTIONS --exclude witness0 builder"
