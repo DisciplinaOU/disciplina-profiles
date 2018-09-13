39d6b4c2298eee27eea9098f2a05522c257305cf
Yorick van Pelt <yorick@yorickvanpelt.nl>
2018-09-13 16:41:27 +0200

    redo prometheus config, remove alertmanager and grafana

--- deployments/logical/nodes/builder.nix
+++ deployments/logical/nodes/builder.nix
@@ -1,14 +1,8 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, nodes, ... }:
 let
   keys = config.dscp.keys;
   ports = {
-    grafana = 3000;
     prometheus = 9090;
-    exporters = {
-      node = 9100;
-      nginx = 9113;
-    };
-    alertManager = 9093;
     telegraf = 9273;
     statsd = 8125;
   };
@@ -202,9 +196,6 @@ in
             keepalive 32;
           '';
         };
-        upstreams.grafana.servers."localhost:${toString ports.grafana}" = {};
-        upstreams.prometheus.servers."localhost:${toString ports.prometheus}" = {};
-        upstreams.alertManager.servers."localhost:${toString ports.alertManager}" = {};
 
         virtualHosts = {
           witness = {
@@ -226,9 +217,7 @@ in
             "/".root = "${pkgs.disciplina-explorer-frontend}";
           };
       
-          grafana.locations."/".proxyPass = "http://grafana";
-          prometheus.locations."/".proxyPass = "http://prometheus";
-          alertManager.locations."/".proxyPass = "http://alertManager";
+          prometheus.locations."/".proxyPass = "http://localhost:${toString ports.prometheus}";
         };
       };
 
@@ -238,77 +227,29 @@ in
         extraFlags = [
           "--web.enable-admin-api"
         ];
-        configText = ''
-          global:
-          scrape_interval: 15s
-
-          alerting:
-          alertmanagers:
-          - static_configs:
-          - targets:
-          - 'localhost:${toString ports.alertManager}'
-
-          rule_files:
-          - ${rulesFile}
-
-          scrape_configs:
-          - job_name: 'node'
-          static_configs:
-          - targets:
-          - 'builder:${toString ports.exporters.node}'
-          - 'witness0:${toString ports.exporters.node}'
-          - 'witness1:${toString ports.exporters.node}'
-          - 'witness2:${toString ports.exporters.node}'
-          - 'witness3:${toString ports.exporters.node}'
-          - job_name: 'telegraf'
-          static_configs:
-          - targets:
-          - 'builder:${toString ports.telegraf}'
-          - job_name: 'prometheus'
-          static_configs:
-          - targets:
-          - 'builder:${toString ports.prometheus}'
-        '';
-        alertmanager =
-          {
-            enable = true;
-            # webExternalUrl = with config.services.nginx.virtualHosts.alertManager;
-              #   "http${lib.optionalString (enableSSL || onlySSL || addSSL || forceSSL) "s"}://${serverName}/";
-            webExternalUrl = "https://alertmanager.net.disciplina.io";
-
-            configuration = {
-              global = {
-                slack_api_url = "https://hooks.slack.com/services/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";
-                smtp_from = "monitoring@serokell.io";
-                smtp_smarthost = "smtp.gmail.com:587";
-                smtp_hello = "builder.net.disciplina.io";
-                smtp_auth_username = "monitoring@serokell.io";
-                smtp_auth_password = "mjusezftwgdnpisi";
-              };
-              receivers = [
-                {
-                  name = "ops";
-                  slack_configs = [
-                    {
-                      send_resolved = true;
-                      channel = "#devops-alerts-dscp";
-                      username = "prometheus";
-                    }
-                  ];
-                  email_configs = [
-                    {
-                      to = "operations@serokell.io";
-                    }
-                  ];
-                }
-              ];
-
-              route = {
-                receiver = "ops";
-              };
+        configText = pkgs.writeText "prometheus.yaml" (builtins.toJSON {
+          global = { scrape_interval = "15s"; };
+          alerting.alertmanagers = [ {
+            static_config.targets = [ "alertmanager.serokell.io" ];
+            scheme = "https";
+            basic_auth = {
+              username = "alertmanager";
+              password_file = toString keys.alertmanager_token;
             };
-          };
-
+          } ];
+          rule_files = [ "${rulesFile}" ];
+          scrape_configs = [
+            { job_name = "node";
+              static_configs.targets =
+                lib.mapAttrsToList
+                  (name: node: "${name}:${toString node.config.services.prometheus.exporters.node.port}")
+                  nodes; }
+            { job_name = "telegraf";
+              static_configs.targets = ["localhost:${toString ports.telegraf}"] }
+            { job_name = "prometheus";
+              static_configs.targets = ["localhost:${toString ports.prometheus}"] }
+          ];
+        });
       };
 
       services.telegraf = {
@@ -329,29 +270,11 @@ in
         };
       };
 
-      services.grafana = {
-        enable = true;
-        port = ports.grafana;
-        addr = "0.0.0.0";
-        rootUrl = "https://grafana.net.disciplina.io";
-        # extraOptions = {
-          #   AUTH_GOOGLE_ENABLED = "true";
-          #   AUTH_GOOGLE_ALLOWED_DOMAINS = "serokell.io";
-          #   AUTH_GOOGLE_ALLOW_SIGN_UP = "true";
-          # };
-      };
-
-      # systemd.services.grafana.serviceConfig = {
-        #   PermissionsStartOnly = true;
-        #   # EnvironmentFile = "/root/grafana.env";
-        #   # EnvironmentFile = keys.grafana-env;
-        # };
-
       dscp.keys = {
+        alertmanager-token =    { services = [ "alertmanager" ]; user = "alertmanager"; };
         buildkite-token =       { services = [ "buildkite-agent" ]; user = "buildkite-agent"; };
         buildkite-ssh-private = { services = [ "buildkite-agent" ]; user = "buildkite-agent"; };
         buildkite-ssh-public =  { services = [ "buildkite-agent" ]; user = "buildkite-agent"; };
-        # grafana-env     = { user = "grafana"; services = [ "grafana" ]; };
         aws-credentials = { user = "nixops"; shared = false; };
         faucet-keyfile  = { user = "disciplina"; services = [ "disciplina-faucet" ]; };
       };
